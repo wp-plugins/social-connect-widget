4f08d72072bac2550d4078d2e42185ede6cbb467
scrybes <scrybes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-30 23:19:17 +0000

    Adding first version of Social Connect Widget plugin
    git-svn-id: https://plugins.svn.wordpress.org/social-connect-widget/trunk@482592 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,57 @@
+=== Social Connect Widget ===
+Contributors: Scrybes WordPress Hosting
+Donate link: http://scryb.es/
+Tags: social, twitter, facebook, rss, footer, google plus, widget
+Requires at least: 2.0.2
+Tested up to: 3.3.0
+Stable tag: 4.3
+
+A simple plug-in that provides a widget to display pretty little (30x30 px) icons with links to your Facebook, Twitter, Google+ and RSS (Feedburner) pages.
+
+== Description ==
+
+This plugin will help you to display links to your Facebook, Twitter, Google+ and RSS (Feedburner) feeds with nice little icons.
+
+These days it's essentially a necessity to operate hub pages on the major social networks, both as a way to improve a sites SEO and to communicate with users and customers across a range of mediums. This plugin will help you to display pretty links to your WordPress Blog or Sites Twitter, Facebook, Google+ and Feedburner (RSS) pages.
+
+At the time of writing we appreciate this plugin is in its infancy and has a very specific usefulness. Stick with us however as we plan to add many more configurable options as part of future versions and we'd love to hear your thoughts via the contact details below. 
+
+== Installation ==
+
+Installation Steps
+
+1. Upload the Social Connect Widget folder to the /wp-content/plugins/ directory via FTP. <strong>OR</strong> Add the Social Connect Widget Plugin via the WordPress Plugins menu.
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Add the Widget to the desired footer in the WordPress Widgets menu.
+4. Insert your Twitter username, Facebook URL tag, Google+ full url and Feedburner ID and hit save
+5. Rate the plugin and verify that it works at wordpress.org.
+6. Leave a comment with Scrybes regarding bugs, feature request etc...
+
+Some Formatting Tips
+
+* If you know CSS then you can change the spacing between icons according to your WordPress Blog/Site. Go to WP Dashboard>Plugins>Editor> select Social Connections Widget > Edit social-connect-widget/css/style.css
+
+== Frequently Asked Questions ==
+
+= Where is the demo? =
+Here at the bottom of the page
+<a href="http://scryb.es">http://scryb.es</a>
+
+= How do I contact you if I have questions/suggestion? =
+Please contact us via our website:
+
+<a href="http://scryb.es">Scrybes WordPress Hosting</a>
+
+== Screenshots ==
+
+1. Widget in use screenshot-1.png
+2. Widget setup options screenshot-2.png
+
+== Changelog ==
+
+= 1.0 =
+* Version 1.0 of Social Connection Widget is the first version available as a WordPress plugin.
+
+== Upgrade Notice ==
+
+= 1.0 =
---
+++ socialConnect.php
@@ -0,0 +1,120 @@
+<?php
+/*
+ * Plugin Name: Social Connections Widget
+ * Version: 1.0
+ * Plugin URI: http://scryb.es/
+ * Description: A widget designed to easily add icons and links to your social pages on all the major networks.
+ * Author: Scrybes WordPress Hosting
+ * Author URI: http://scryb.es/
+ */
+?>
+<?php
+function addHeaderCode() {
+	echo '<link type="text/css" rel="stylesheet" href="' . get_bloginfo('wpurl') . '/wp-content/plugins/social-connect-widget/css/style.css" />' . "\n";
+}
+function my_plugin_create_table()
+{
+	global $wpdb; 
+	if($wpdb->get_var("show tables like SCW_Stats") != 'SCW_Stats') 
+	{
+		$sql = "CREATE TABLE SCW_Stats (
+		id mediumint(9) NOT NULL,
+		facebook tinytext NOT NULL,
+		twitter tinytext NOT NULL,
+                googleplus tinytext NOT NULL,
+		rss tinytext NOT NULL,
+		UNIQUE KEY id (id)
+		);";
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($sql);
+	}
+}
+register_activation_hook( __FILE__, 'my_plugin_create_table' );
+class scWidget extends WP_Widget
+{
+	function scWidget(){
+		$widget_ops = array('classname' => 'widget_hello_world', 'description' => __( "This widget allows you to easily add icons and links to your social network pages. Best used in the footer areas.") );
+		$control_ops = array('width' => 300, 'height' => 300);
+		$this->WP_Widget('helloworld', __('Social Connection Links Widget'), $widget_ops, $control_ops);
+	}	
+	function widget($args, $instance){
+		extract($args);
+		$title = apply_filters('widget_title', $instance['title'] );
+		$facebook = empty($instance['facebook']) ? 'scrybes' : $instance['facebook'];
+		$twitter = empty($instance['twitter']) ? 'scrybes' : $instance['twitter'];
+                $googleplus = empty($instance['googleplus']) ? 'http://gplus.to/scryb.es' : $instance['googleplus'];
+		$rss = empty($instance['rss']) ? 'scrybes' : $instance['rss'];
+		
+		/* Before widget (defined by themes). */
+		echo $before_widget;
+		/* Title of widget (before and after defined by themes). */
+		if ( $title )
+			echo $before_title . $title . $after_title;
+				
+		echo '
+<div>
+        <a target="_blank" href="http://twitter.com/'.$twitter.'" id="followTwitter"><img src="'.get_bloginfo('url').'/wp-content/plugins/social-connect-widget/img/twitter.png" class="scwLogos" alt="Twitter" /></a> 
+
+        <a target="_blank" href="http://www.facebook.com/'.$facebook.'" id="followFacebook"><img src="'.get_bloginfo('url').'/wp-content/plugins/social-connect-widget/img/facebook.png" class="scwLogos" alt="Facebook" /></a> 
+
+        <a target="_blank" href="'.$googleplus.'" id="followGooglePlus"><img src="'.get_bloginfo('url').'/wp-content/plugins/social-connect-widget/img/googleplus.png" class="scwLogos" alt="Google+" /></a>
+ 
+        <a target="_blank" href="http://feeds.feedburner.com/'.$rss.'" id="subscribeRSS"><img src="'.get_bloginfo('url').'/wp-content/plugins/social-connect-widget/img/rss.png" class="scwLogos" alt="RSS"/></a>
+</div>
+';		
+		echo $after_widget;
+	}
+	/*Plugin Update */
+	function update($new_instance, $old_instance){
+		$instance = $old_instance;
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['facebook'] = strip_tags(stripslashes($new_instance['facebook']));
+		$instance['twitter'] = strip_tags(stripslashes($new_instance['twitter']));
+                $instance['googleplus'] = strip_tags(stripslashes($new_instance['googleplus']));
+		$instance['rss'] = strip_tags(stripslashes($new_instance['rss']));		
+		global $wpdb;
+			$wpdb->insert( 'SCW_Stats', array(
+			'id'	=> 1,
+			'facebook' => $instance['facebook'], 
+			'twitter' => $instance['twitter'],
+                        'googleplus' => $instance['googleplus'],
+			'rss' => $instance['rss']
+			) 
+		);		
+		global $wpdb;
+			$wpdb->update( 'SCW_Stats', 
+			array( 
+				'facebook' => $instance['facebook'], 
+				'twitter' => $instance['twitter'],
+                                'googleplus' => $instance['googleplus'],
+				'rss' => $instance['rss']
+			),
+			array(
+				'id' => 1
+			) 
+		);	
+		return $instance;
+	}	
+	function form($instance){
+		$instance = wp_parse_args( (array) $instance, array('facebook'=>'scrybes', 'twitter'=>'scrybes', 'googleplus'=>'scrybes', 'rss'=>'scrybes') );		
+		$facebook = htmlspecialchars($instance['facebook']);
+		$twitter = htmlspecialchars($instance['twitter']);
+                $googleplus = htmlspecialchars($instance['googleplus']);
+		$rss = htmlspecialchars($instance['rss']);	
+		?>
+			<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title:', 'hybrid'); ?></label>
+			<input id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" style="width:100%;" />
+			</p>
+		<?php
+		echo '<p><label for="' . $this->get_field_name('facebook') . '">' . ('Facebook URL handle:') . ' <input id="' . $this->get_field_id('facebook') . '" name="' . $this->get_field_name('facebook') . '" type="text" value="' . $facebook . '" style="width:100%;"/></label></p>';
+		echo '<p><label for="' . $this->get_field_name('twitter') . '">' . ('Twitter handle:') . ' <input id="' . $this->get_field_id('twitter') . '" name="' . $this->get_field_name('twitter') . '" type="text" value="' . $twitter . '" style="width:100%;"/></label></p>';
+		echo '<p><label for="' . $this->get_field_name('googleplus') . '">' . ('Google+ URL:') . ' <input id="' . $this->get_field_id('googleplus') . '" name="' . $this->get_field_name('googleplus') . '" type="text" value="' . $googleplus . '" style="width:100%;"/></label></p>';
+		echo '<p><label for="' . $this->get_field_name('rss') . '">' . __('Feedburner name:') . ' <input style="width:100%;" id="' . $this->get_field_id('rss') . '" name="' . $this->get_field_name('rss') . '" type="text" value="' . $rss . '" /></label></p>';
+	}
+}
+function SC_Widget() {
+	register_widget('scWidget');
+}	
+add_action('widgets_init', 'SC_Widget');
+add_action('wp_head', 'addHeaderCode');
+?>
