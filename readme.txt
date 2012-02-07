9447adf4f52ca0137ed79da361f65dfdd938ed5e
scrybes <scrybes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-07 10:05:54 +0000

    MAJOR UPDATE. Code rewrite, support for LinkedIn, new icons, bug fixes, new options page
    git-svn-id: https://plugins.svn.wordpress.org/social-connect-widget/trunk@501552 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,52 +1,60 @@
 === Social Connect Widget ===
 Contributors: Scrybes WordPress Hosting
 Donate link: http://scryb.es/
-Tags: social, twitter, facebook, rss, footer, google plus, you tube, tumblr, widget
+Tags: social, twitter, facebook, rss, footer, google plus, you tube, tumblr, linkedin, widget
 Requires at least: 2.0.2
 Tested up to: 3.3.0
 Stable tag: 4.3
 
-A simple plug-in that provides a widget to display nice little icons with links to your Facebook, Twitter, Google+, YouTube, Tumblr and RSS (Feedburner) pages.
+A simple plug-in that provides a widget to display nice little icons with links to most of the popular social network sites.
 
 == Description ==
 
-This plugin will help you to display links to your Facebook, Twitter, Google+, YouTube, Tumblr and RSS (Feedburner) feeds with nice little icons.
+This plugin will simplify adding links (nice little icons) to Facebook, Twitter, Google+, YouTube, Tumblr, LinkedIn and RSS feeds from your WordPress powered site.
 
-These days it's essentially a necessity to operate hub pages on the major social networks, both as a way to improve a sites SEO and to communicate with users and customers across a range of mediums. This plugin will help you to display pretty links to your WordPress Blog or Sites Twitter, Facebook, Google+, YouTube, Tumblr and Feedburner (RSS) pages.
+These days it's essentially a necessity to operate hub pages on the major social networks, both as a way to improve a sites SEO and to communicate with users and customers across a range of mediums. This plugin will help you to display pretty links to your WordPress Blog or Sites Twitter, Facebook, Google+, YouTube, Tumblr, LinkedIn and RSS feed pages.
 
-We plan to continue adding many more configurable options in future versions and we'd love to hear your thoughts via the contact details below. If you ask nicely, we'll probably make it happen! 
+Further configuration options definitely coming in future versions and we'd love to hear your thoughts via the contact details below. If you ask nicely, we'll probably make it happen! 
 
 == Installation ==
 
 Installation Steps
 
 1. Upload the Social Connect Widget folder to the /wp-content/plugins/ directory via FTP. <strong>OR</strong> Add the Social Connect Widget Plugin via the WordPress Plugins menu.
-2. Activate the plugin through the 'Plugins' menu in WordPress
+2. Activate the plugin through the 'Plugins' menu in WordPress.
 3. Add the Widget to the desired area in the WordPress Widgets menu.
-4. Insert your Twitter username, Facebook URL tag, Google+ full url, YouTube channel name, Tumblr URL name and Feedburner ID and hit save
-5. Note that if you don't want a particular icon to be displayed simply make the corresponding text field empty
+4. Go to Settings > Social Connect Widget to configure the various options.
+5. Note that if you don't want a particular icon to be displayed simply make the corresponding text field empty.
 6. Rate the plugin and verify that it works at wordpress.org.
 7. Leave a comment with Scrybes regarding bugs, feature request etc...
 
 Some Formatting Tips
 
-* If you would rather not display one of the icons simply leave the corresponding text area blank on the widget and it won't be shown.
-* If you know CSS then you can change the spacing between icons according to your WordPress Blog/Site. Go to WP Dashboard>Plugins>Editor> select Social Connections Widget > Edit social-connect-widget/css/style.css
+* If you would rather not display one of the icons simply leave the corresponding text area blank on the settings page and it won't be shown.
+* If you know CSS then you can change the spacing between icons according to your WordPress Blog/Site. Go to WP Dashboard > Plugins > Editor > select Social Connections Widget > Edit social-connect-widget/css/socialConnect-style.css
 
 == Frequently Asked Questions ==
 
 = Where is the demo? =
-On Scrybes <a href="http://scryb.es">WordPress hosting site</a>, at the bottom of the page
+On <a href="http://scryb.es">Scrybes WordPress hosting</a> site, at the bottom of the page
 
 = How do I contact you if I have questions/suggestion? =
 Please contact us via the <a href="http://scryb.es/contact">Scrybes website</a>, we'd love to hear from you
 
 == Screenshots ==
 
-1. Widget in use screenshot-1.png (v 1.0)
-2. Widget setup options screenshot-2.png (v 1.0)
+1. Plugin in use screenshot-1.png (v 1.3)
+2. Set-up options screen screenshot-2.png (v 1.3)
 
 == Changelog ==
+
+= 1.3 =
+* MAJOR UPDATE!
+* Complete code rewrite
+* Moved options to the WordPress settings menu (if upgrading from v 1.2 re-configure)
+* Added support for LinkedIn
+* New, improved <a href="http://www.elegantthemes.com/affiliates/idevaffiliate.php?id=13301_0_1_10">elegant themes</a> icon set
+* Various bug fixes (thanks Eric)
 
 = 1.2 =
 * Added support for Tumblr at the request of Miss McBooty over at <a href="http://foodiemcbooty.com/">foodiemcbooty.com</a>
--- socialConnect.php
+++ socialConnect.php
@@ -1,145 +1,42 @@
 <?php
 /*
- * Plugin Name: Social Connections Widget
- * Version: 1.2
+ * Plugin Name: Social Connect Widget
+ * Version: 1.3
  * Plugin URI: http://scryb.es/
- * Description: A widget designed to easily add icons and links to your social pages on all the major networks.
+ * Description: A widget designed to easily add icons with links to your social pages on all the major networks.
  * Author: Scrybes WordPress Hosting
  * Author URI: http://scryb.es/
  */
 ?>
 <?php
-function addHeaderCode() {
-	echo '<link type="text/css" rel="stylesheet" href="' . get_bloginfo('wpurl') . '/wp-content/plugins/social-connect-widget/css/style.css" />' . "\n";
+//Add necessary includes
+require_once(dirname(__FILE__) . '/inc/output.php');
+require_once(dirname(__FILE__) . '/inc/functions.php');
+require_once(dirname(__FILE__) . '/inc/settings.php');
+require_once(dirname(__FILE__) . '/inc/widget.php');
+
+// Runs when plugin is activated
+register_activation_hook(__FILE__,'socialConnect_install'); 
+
+// Runs on plugin deactivation
+register_deactivation_hook( __FILE__, 'socialConnect_remove' );
+
+//Determine if the plugin has been being updated from version 1.2 and remove depreciated table
+global $wpdb;
+if($wpdb->get_var("SHOW TABLES LIKE 'SCW_Stats';") == 'SCW_Stats') {
+	$wpdb->query("DROP TABLE SCW_Stats");
 }
-function my_plugin_create_table()
-{
-	global $wpdb; 
-	if($wpdb->get_var("show tables like SCW_Stats") != 'SCW_Stats') 
-	{
-		$sql = "CREATE TABLE SCW_Stats (
-		id mediumint(9) NOT NULL,
-		facebook tinytext NOT NULL,
-		twitter tinytext NOT NULL,
-                googleplus tinytext NOT NULL,
-                youtube tinytext NOT NULL,
-		tumblr tinytext NOT NULL,
-		rss tinytext NOT NULL,
-		UNIQUE KEY id (id)
-		);";
-		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-		dbDelta($sql);
-	}
-}
-register_activation_hook( __FILE__, 'my_plugin_create_table' );
-class scWidget extends WP_Widget
-{
-	function scWidget(){
-		$widget_ops = array('classname' => 'widget_hello_world', 'description' => __( "Add icons and links to your social network pages. Works nicely in footer areas. Simply leave fields blank to hide icons.") );
-		$control_ops = array('width' => 300, 'height' => 300);
-		$this->WP_Widget('helloworld', __('Social Connection Links Widget'), $widget_ops, $control_ops);
-	}	
-	function widget($args, $instance){
-		extract($args);
-		$title = apply_filters('widget_title', $instance['title'] );
-		$facebook = empty($instance['facebook']) ? '' : $instance['facebook'];
-		$twitter = empty($instance['twitter']) ? '' : $instance['twitter'];
-                $googleplus = empty($instance['googleplus']) ? '' : $instance['googleplus'];
-                $youtube = empty($instance['youtube']) ? '' : $instance['youtube'];
-		$tumblr = empty($instance['tumblr']) ? '' : $instance['tumblr'];
-		$rss = empty($instance['rss']) ? '' : $instance['rss'];
-		
-		/* Before widget (defined by themes). */
-		echo $before_widget;
-		/* Title of widget (before and after defined by themes). */
-		if ( $title )
-			echo $before_title . $title . $after_title;
-				
-		echo '<div> ';
-		
-if (!($twitter==''))
-{ echo  '<a target="_blank" href="http://twitter.com/'.$twitter.'" id="followTwitter"><img src="'.get_bloginfo('url').'/wp-content/plugins/social-connect-widget/img/twitter.png" class="scwLogos" alt="Twitter" /></a>' ; }
 
-if (!($facebook=='')) 
-{ echo '<a target="_blank" href="http://www.facebook.com/'.$facebook.'" id="followFacebook"><img src="'.get_bloginfo('url').'/wp-content/plugins/social-connect-widget/img/facebook.png" class="scwLogos" alt="Facebook" /></a>' ; }
+//Display admin messages
+add_action('admin_notices', 'socialConnect_adminNotice');
+add_action('admin_init', 'socialConnect_adminNotice_ignore');
 
-if (!($googleplus=='')) 
-{ echo '<a target="_blank" href="'.$googleplus.'" id="followGooglePlus"><img src="'.get_bloginfo('url').'/wp-content/plugins/social-connect-widget/img/googleplus.png" class="scwLogos" alt="Google+" /></a>' ; }
+// Add the widget	
+add_action('widgets_init', 'socialConnect_registerWidget');
 
-if (!($youtube=='')) 
-{ echo '<a target="_blank" href="http://www.youtube.com/'.$youtube.'" id="followYouTube"><img src="'.get_bloginfo('url').'/wp-content/plugins/social-connect-widget/img/youtube.png" class="scwLogos" alt="YouTube" /></a>' ; }
-
-if (!($tumblr=='')) 
-{ echo '<a target="_blank" href="http://'.$tumblr.'.tumblr.com" id="followTumblr"><img src="'.get_bloginfo('url').'/wp-content/plugins/social-connect-widget/img/tumblr.png" class="scwLogos" alt="Tumblr" /></a>' ; }
-
-if (!($rss=='')) 
-{ echo '<a target="_blank" href="http://feeds.feedburner.com/'.$rss.'" id="subscribeRSS"><img src="'.get_bloginfo('url').'/wp-content/plugins/social-connect-widget/img/rss.png" class="scwLogos" alt="RSS"/></a>' ; }
-
-   echo '</div>' ;	
-   	
-		echo $after_widget;
-	}
-	/*Plugin Update */
-	function update($new_instance, $old_instance){
-		$instance = $old_instance;
-		$instance['title'] = strip_tags( $new_instance['title'] );
-		$instance['facebook'] = strip_tags(stripslashes($new_instance['facebook']));
-		$instance['twitter'] = strip_tags(stripslashes($new_instance['twitter']));
-                $instance['googleplus'] = strip_tags(stripslashes($new_instance['googleplus']));
-                $instance['youtube'] = strip_tags(stripslashes($new_instance['youtube']));
-                $instance['tumblr'] = strip_tags(stripslashes($new_instance['tumblr']));
-		$instance['rss'] = strip_tags(stripslashes($new_instance['rss']));		
-		global $wpdb;
-			$wpdb->insert( 'SCW_Stats', array(
-			'id'	=> 1,
-			'facebook' => $instance['facebook'], 
-			'twitter' => $instance['twitter'],
-                        'googleplus' => $instance['googleplus'],
-                        'youtube' => $instance['youtube'],
-			'tumblr' => $instance['tumblr'],
-			'rss' => $instance['rss']
-			) 
-		);		
-		global $wpdb;
-			$wpdb->update( 'SCW_Stats', 
-			array( 
-				'facebook' => $instance['facebook'], 
-				'twitter' => $instance['twitter'],
-                                'googleplus' => $instance['googleplus'],
-                                'youtube' => $instance['youtube'],
-                                'tumblr' => $instance['tumblr'],
-				'rss' => $instance['rss']
-			),
-			array(
-				'id' => 1
-			) 
-		);	
-		return $instance;
-	}	
-	function form($instance){
-		$instance = wp_parse_args( (array) $instance, array('facebook'=>'scrybes', 'twitter'=>'scrybes', 'googleplus'=>'http://gplus.to/scryb.es', 'youtube'=>'', 'tumblr'=>'', 'rss'=>'scrybes') );		
-		$facebook = htmlspecialchars($instance['facebook']);
-		$twitter = htmlspecialchars($instance['twitter']);
-                $googleplus = htmlspecialchars($instance['googleplus']);
-                $youtube = htmlspecialchars($instance['youtube']);
-                $tumblr = htmlspecialchars($instance['tumblr']);
-		$rss = htmlspecialchars($instance['rss']);	
-		?>
-			<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title:', 'hybrid'); ?></label>
-			<input id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" style="width:100%;" />
-			</p>
-		<?php
-		echo '<p><label for="' . $this->get_field_name('facebook') . '">' . ('Facebook URL handle:') . ' <input id="' . $this->get_field_id('facebook') . '" name="' . $this->get_field_name('facebook') . '" type="text" value="' . $facebook . '" style="width:100%;"/></label></p>';
-		echo '<p><label for="' . $this->get_field_name('twitter') . '">' . ('Twitter handle:') . ' <input id="' . $this->get_field_id('twitter') . '" name="' . $this->get_field_name('twitter') . '" type="text" value="' . $twitter . '" style="width:100%;"/></label></p>';
-		echo '<p><label for="' . $this->get_field_name('googleplus') . '">' . ('Google+ URL:') . ' <input id="' . $this->get_field_id('googleplus') . '" name="' . $this->get_field_name('googleplus') . '" type="text" value="' . $googleplus . '" style="width:100%;"/></label></p>';
-		echo '<p><label for="' . $this->get_field_name('youtube') . '">' . ('YouTube Channel Name:') . ' <input id="' . $this->get_field_id('youtube') . '" name="' . $this->get_field_name('youtube') . '" type="text" value="' . $youtube . '" style="width:100%;"/></label></p>';
-		echo '<p><label for="' . $this->get_field_name('tumblr') . '">' . ('Tumblr URL Name:') . ' <input id="' . $this->get_field_id('tumblr') . '" name="' . $this->get_field_name('tumblr') . '" type="text" value="' . $tumblr . '" style="width:100%;"/></label></p>';
-		echo '<p><label for="' . $this->get_field_name('rss') . '">' . __('Feedburner name:') . ' <input style="width:100%;" id="' . $this->get_field_id('rss') . '" name="' . $this->get_field_name('rss') . '" type="text" value="' . $rss . '" /></label></p>';
-	}
-}
-function SC_Widget() {
-	register_widget('scWidget');
-}	
-add_action('widgets_init', 'SC_Widget');
+// Add the header code (css) to WordPress page
 add_action('wp_head', 'addHeaderCode');
+
+//Register the shortcode
+add_shortcode("dnh32pw75j", "socialConnect_shortcodeHandler");
 ?>
